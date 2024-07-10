784ebdca23a459ac43e79941b782d0ac976a96ee
Casa <danilosilvadesouza05@gmail.com>
2024-07-10 09:01:03 -0300

    peimeiro commint danilo

---
+++ condicionamento.php
@@ -0,0 +1,27 @@
+<?php
+$nota = 5;
+if($nota <= 2) {
+    echo 'Nota fraca';
+} elseif ($nota <= 4) {
+    echo 'Nota insuficiente';
+}elseif ($nota <= 6) {
+    echo 'Nota razoavel';
+}elseif ($nota <= 8) {
+    echo 'Nota positiva';
+}else {
+    echo 'Nota excelente';
+}
+
+echo "<hr>"; // quebra de linha
+//switch é usado quando tem varias condições
+$numero = 10;
+switch ($numero){
+    case 8:
+        echo "8";
+        break; // coloca no final de cada instrução
+    case 9;
+        echo "9";
+        break;
+    default: // é igual else
+        echo "Nenhum dos valores";
+}
---
+++ variaveis.php
@@ -0,0 +1,30 @@
+<?php
+
+
+echo "primeira aula variavel <br>";
+
+$variavel =1;
+$variaveldois =2;
+
+echo "variavel um boas vindas {$variavel} <br>";
+echo "variavel dois continuação {$variaveldois} <br>";
+echo 'variavel um boas vindas {$variavel} <br>';
+echo 'variavel dois continuação {$variaveldois} <br>';
+
+$nome ="Danilo";
+$nomedois = "Ana";
+
+echo "olá sr. {$nome} <br>";
+echo "olá sra. {$nomedois} <br>";
+
+$variavelconcat =$nome . " " .$nomedois;
+
+
+echo $variavelconcat . "<br>";
+
+$variavel3 = $variavel + $variaveldois ;
+
+echo $variavel3 . "<br>";
+
+
+
