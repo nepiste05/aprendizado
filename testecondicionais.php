f09ce534b775da569138a426cc61716809d548f6
Casa <danilosilvadesouza05@gmail.com>
2024-07-14 21:03:50 -0300

    terceiro teste de Alberto

--- testecondicionais.php
+++ testecondicionais.php
@@ -33,22 +33,31 @@ switch ($dia) {
     case 'domingo':
         echo "Domingo";
     break; }
+
             echo "<br>";
 
 // desafio 3 Laço while
 
 $numero = 1;
 while ($numero <= 10) {
-    echo "Numero: $numero\n";
+    echo "Numero: $numero <br>";
     $numero++;
 }
     echo "<br>";
 
 //desafio 4 Laço do-while
 
+$numero = 11;
+do {
+    echo "Digite um numero maior que 10: <Br>";
 
+    if ($numero <= 10) {
+        echo "Numero é menor ou igual a 10.\n";
+    }   break;
+} while ($numero <= 10);
+    echo "Numero digitado é maior que 10:$numero\n";
+    "break\n";
 echo "<br>";
-
 
 // desafio 5 Laço for
 
